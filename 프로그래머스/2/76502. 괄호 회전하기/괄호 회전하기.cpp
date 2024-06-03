2474c193872aaba76e8a994cc150be3e1e6936fc
kisub Lee <49126967+cagongman@users.noreply.github.com>
2024-06-03 16:53:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+
+using namespace std;
+
+
+bool CheckPop(char open, char close)
+{
+    switch(open)
+    {
+        case '{':
+            if(close == '}')
+                return true;
+            else
+                return false;
+        case '(':
+            if(close == ')')
+                return true;
+            else
+                return false;
+        case '[':
+            if(close == ']')
+                return true;
+            else
+                return false;
+    }
+}
+
+int solution(string s) {
+    int answer = 0;
+    list<char> Brackets;
+    
+    for(char c : s)
+        Brackets.push_back(c);
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(i != 0)
+        {
+            int temp;
+            temp = Brackets.front();
+            Brackets.pop_front();
+            Brackets.push_back(temp);
+        }
+        
+        stack<char> check;
+        
+        for(auto value : Brackets)
+        {
+            if(check.empty())
+            {
+                check.push(value);
+            }
+            else
+            {
+                if(CheckPop(check.top(), value))
+                {
+                    check.pop();
+                }
+                else
+                {
+                    check.push(value);
+                }
+            }
+        }
+        
+        if(check.empty())
+            answer++;
+    }
+        
+    return answer;
+}
