de9cb621291d66c48bcf6c20e4258076eed4ea16
Kisub Lee <49126967+cagongman@users.noreply.github.com>
2024-07-15 21:30:16 +0900

    [level 2] Title: 주식가격, Time: 26.67 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,45 +7,23 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size(), 0);
     stack<int> s;
-    int index;
-    
-    for(int i = 0; i < prices.size() - 1; i++)
-    {
-        if(s.empty())
-        {
-            s.push(i);
-            continue;
+
+    for (int i = 0; i < prices.size(); i++) {
+        // 스택이 비어있지 않고, 현재 가격이 스택의 탑 가격보다 낮을 때
+        while (!s.empty() && prices[s.top()] > prices[i]) {
+            int top = s.top();
+            s.pop();
+            answer[top] = i - top; // 떨어진 시점 계산
         }
-    
-        index = s.top();
-        
-        // 주식 값이 떨어졌을때
-        if(prices[index] > prices[i])
-        {
-            while(!s.empty())
-            {
-                answer[index] = i - index;
-                s.pop();
-                
-                if(!s.empty())
-                {
-                    index = s.top();
-                
-                    if(prices[index] <= prices[i])
-                        break;
-                }
-            }
-        }
-        
-        s.push(i);
+        s.push(i); // 현재 인덱스를 스택에 추가
     }
-    
-    while(!s.empty())
-    {
-        answer[s.top()] = prices.size() - s.top() - 1;
+
+    // 끝까지 가격이 떨어지지 않은 경우 처리
+    while (!s.empty()) {
+        int top = s.top();
         s.pop();
+        answer[top] = prices.size() - top - 1;
     }
-        
-    
+
     return answer;
 }
