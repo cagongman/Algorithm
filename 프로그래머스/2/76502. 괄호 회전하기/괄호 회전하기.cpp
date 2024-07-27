d204554810cf68a2751bb6533e36358f0b4cf327
Kisub Lee <49126967+cagongman@users.noreply.github.com>
2024-07-27 20:38:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 8.94 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,49 +1,46 @@
+#include <stack>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-bool IsBracket(char front, char back)
-{
-    if (front == '(' && back == ')') return true;
-    if (front == '[' && back == ']') return true;
-    if (front == '{' && back == '}') return true;
-    return false;
-}
+unordered_map<char, char> bracketPair = {{')', '('}, {'}', '{'}, {']', '['}};
 
-bool IsBracketString(string str)
+bool isValid(string &s, int start)
 {
-    if (str.empty())
-        return true;
-
-    bool loop = false;
-    for (int i = 1; i < str.size(); ++i)
+    stack<char> stk;
+    unsigned int sz = s.size();
+    
+    for(int i = 0; i < sz; i++)
     {
-        if (IsBracket(str[i - 1], str[i]))
+        char ch = s[(start + i) % sz];
+        
+        // ch가 닫힌 괄호인 경우
+        if(bracketPair.count(ch))
         {
-            str.erase(i - 1, 2);
-            loop = true;
+            if(stk.empty() || stk.top() != bracketPair[ch])
+                return false;
+            
+            // ch와 짝이 맞는 열린 괄호면 해당 열린 괄호 제거
+            stk.pop();
+        }
+        else
+        {
+            stk.push(ch);
         }
     }
-
-    if (loop)
-        return IsBracketString(str);
-    return false;
+    
+    return stk.empty(); 
 }
 
-int solution(string s) {
+int solution(string s)
+{
     int answer = 0;
-    int loopCount = s.size();
-
-    for (int i = 0; i < loopCount; ++i)
-    {
-        if (IsBracketString(s))
-            ++answer;
-
-        char front = s.front();
-        s.erase(0, 1);
-        s.push_back(front);
-    }
-
+    int n = s.size();
+    
+    for(int i = 0; i < s.size(); i++)
+        answer += isValid(s, i);
+    
     return answer;
 }
